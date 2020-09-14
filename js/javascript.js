1f9c81c36812fcc4c9cd21974eded04375e766ef
KI-Shadow <ivankozlitin2012@gmail.com>
2020-09-14 16:08:57 +0300

    Add progress bar

--- js/javascript.js
+++ js/javascript.js
@@ -5,3 +5,11 @@ $('.j-button-1').click(function() {
     $('.j-alert').hide();
   }, 2000);
 });
+
+let newprogress = 75;
+
+$('.j-button-2').click(function() {
+	newprogress = $('#exampleInputEmail1').val();
+	console.log(newprogress);
+	$('.j-progress-bar').attr('aria-valuenow', newprogress).css('width', newprogress+'%');
+});
